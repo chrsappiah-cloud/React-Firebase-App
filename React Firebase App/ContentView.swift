07ac6d47e0131efc0edb098232cc551c8621b214
Christopher Appiah-Thompson <chrsappiah@gmail.com>
2026-03-23 13:26:05 +1100

    Initial Commit

---
+++ React Firebase App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  React Firebase App
+//
+//  Created by Christopher Appiah-Thompson  on 23/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ React Firebase App/React_Firebase_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  React_Firebase_AppApp.swift
+//  React Firebase App
+//
+//  Created by Christopher Appiah-Thompson  on 23/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct React_Firebase_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
